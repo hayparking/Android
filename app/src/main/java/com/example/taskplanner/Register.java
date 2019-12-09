666474f1c3fc1aba73cde382cf9f256756d04e94
Julian Mateo Guzman Diaz <matguz96@hotmail.com>
2019-12-08 21:19:53 -0500

    Arreglo de variable en register

--- app/src/main/java/com/example/taskplanner/Register.java
+++ app/src/main/java/com/example/taskplanner/Register.java
@@ -4,19 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.taskplanner.Model.User;
+import com.example.taskplanner.Service.UserService;
+import com.example.taskplanner.util.RetrofitHttp;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import retrofit2.Response;
 
 public class Register extends AppCompatActivity {
 
     Button registry;
     EditText name, last_name, email, password;
 
+    private RetrofitHttp retrofitHttp;
+    private static UserService userService;
+    private final ExecutorService executorService =
+            Executors.newFixedThreadPool(1);
+    String nameT;
+    String lastNameT;
+    String emailT;
+    String passwordT;
+    String passwordCOnfirmT;
+    String direccionT;
+    Intent myIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        userService = retrofitHttp.getRetrofit().create(UserService.class);
 
         name = (EditText) findViewById(R.id.nameR);
         last_name = (EditText) findViewById(R.id.lastNameR);
@@ -43,7 +68,33 @@ public class Register extends AppCompatActivity {
                     password.setError("contraseña muy corta");
                 }
                 else {
-                    startActivity(new Intent(v.getContext(), HomeActivity.class));
+                    nameT = name.getText().toString();
+                    lastNameT = last_name.getText().toString();
+                    emailT = email.getText().toString();
+                    passwordT = password.getText().toString();
+                    passwordCOnfirmT = password.getText().toString();
+                    direccionT = "calle 80";
+                    register(v);
+                    //startActivity(new Intent(v.getContext(), HomeActivity.class));
+                    Intent myIntent = new Intent(v.getContext(), HomeActivity.class);
+                }
+            }
+        });
+    }
+
+    public void register (final View view){
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    User userNew = new User(nameT,lastNameT,direccionT,emailT,passwordT,passwordCOnfirmT);
+                    Response<User> userResponse = userService.createUser(userNew).execute();
+                    if (userResponse.isSuccessful()) {
+                        startActivity(myIntent);
+                    }
+                    Log.d("holaaa usuario ", "onClick: "+ userResponse.body().getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
--- app/src/main/java/com/example/taskplanner/Service/UserService.java
+++ app/src/main/java/com/example/taskplanner/Service/UserService.java
@@ -5,12 +5,17 @@ package com.example.taskplanner.Service;
 import com.example.taskplanner.Model.User;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface UserService {
 
     @GET("/api/v1/user/{id}")
     Call<User> getUserByEmail(@Path("id") String email);
+
+    @POST("/api/v1/user")
+    Call<User> createUser(@Body User user);
 
 }
